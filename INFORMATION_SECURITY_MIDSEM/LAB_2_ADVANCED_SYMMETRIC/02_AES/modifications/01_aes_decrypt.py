a2f29ebe129a575756e9b6347b183fbfc9e22537
∇×𝐸 <86531959+Abhijeet-Chandra@users.noreply.github.com>
2026-09-16 20:14:40 +0530

    Update 01_aes_decrypt.py

--- INFORMATION_SECURITY_MIDSEM/LAB_2_ADVANCED_SYMMETRIC/02_AES/modifications/01_aes_decrypt.py
+++ INFORMATION_SECURITY_MIDSEM/LAB_2_ADVANCED_SYMMETRIC/02_AES/modifications/01_aes_decrypt.py
@@ -1,34 +1,26 @@
 from Crypto.Cipher import AES
-from Crypto.Util.Padding import pad, unpad
+from Crypto.Util.Padding import unpad
 
-# AES-128 key written as HEX
-key = bytes.fromhex("0123456789ABCDEF0123456789ABCDEF")
+# AES-128 key
+key = b"0123456789ABCDEF"
 
-# Take plaintext in HEX
-hex_plaintext = input("Enter plaintext in HEX: ")
+# Take ciphertext input in HEX
+ciphertext_hex = input("Enter ciphertext (HEX): ")
 
-# Convert HEX to bytes
-plaintext = bytes.fromhex(hex_plaintext)
+# Convert HEX string to bytes
+ciphertext = bytes.fromhex(ciphertext_hex)
 
 # Create AES cipher
 cipher = AES.new(key, AES.MODE_ECB)
 
-# Pad plaintext
-padded_text = pad(plaintext, AES.block_size)
-
-# Encrypt
-ciphertext = cipher.encrypt(padded_text)
-
-# Display ciphertext in HEX
-print("Encrypted (HEX):", ciphertext.hex())
-
-# Decryption
-cipher = AES.new(key, AES.MODE_ECB)
-
-decrypted = cipher.decrypt(ciphertext)
+# Decrypt
+padded_text = cipher.decrypt(ciphertext)
 
 # Remove padding
-decrypted = unpad(decrypted, AES.block_size)
+plaintext = unpad(padded_text, AES.block_size)
 
-# Display decrypted data
-print("Decrypted (HEX):", decrypted.hex())
+# Convert bytes to string
+plaintext = plaintext.decode()
+
+# Display plaintext
+print("Decrypted:", plaintext)
