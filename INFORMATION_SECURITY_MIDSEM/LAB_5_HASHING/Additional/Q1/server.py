59186ebf441d283d469405bf153baa919c68cf98
∇×𝐸 <86531959+Abhijeet-Chandra@users.noreply.github.com>
2026-09-18 10:40:01 +0530

    lab 5 additional Q1

---
+++ INFORMATION_SECURITY_MIDSEM/LAB_5_HASHING/Additional/Q1/server.py
@@ -0,0 +1,44 @@
+import socket
+
+
+def hash_function(text):
+ hash_value = 5381
+
+ for ch in text:
+
+  hash_value = hash_value * 33 + ord(ch)
+
+  hash_value = hash_value ^ (hash_value >> 16)
+
+  hash_value = hash_value & 0xFFFFFFFF
+
+ return hash_value
+
+data=''
+server = socket.socket()
+
+server.bind(("localhost", 5000))
+server.listen(1)
+
+print("Waiting for client...")
+
+con,addr = server.accept()
+
+
+print("Client connected")
+while(True):
+    part = con.recv(1024).decode()
+    if not part:
+        break
+    print("Received part: ", part)
+    data=data+part
+
+hash_value = hash_function(data)
+
+print("Hash: ", hash_value)
+
+con.send(str(hash_value).encode())
+
+con.close()
+
+server.close()
